70c4f370d9f1e04737a05b90449c7ac782182814
Darlan Marangoni <marangoni.darlan1@gmail.com>
2023-05-22 22:49:21 -0300

    criando a primeira view

--- app/src/main/java/com/darlanmarangoni/MainActivity.kt
+++ app/src/main/java/com/darlanmarangoni/MainActivity.kt
@@ -2,12 +2,16 @@ package com.darlanmarangoni
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo ao Orgs", Toast.LENGTH_SHORT).show()
+        //definindo a view
+        val textView = TextView(this)
+        textView.text = "Cesta de frutas"
+        //mostrando o conteúdo dentro da activity
+        setContentView(textView)
     }
 }
