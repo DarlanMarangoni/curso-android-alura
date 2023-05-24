14fd31d6179c0820dc6751ce905373e20002cb39
Darlan Marangoni <marangoni.darlan1@gmail.com>
2023-05-24 17:20:27 -0300

    Criando uma activity com o assistente e conhecendo o androidx

---
+++ app/src/main/java/com/darlanmarangoni/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,7 @@
+package com.darlanmarangoni.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.darlanmarangoni.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+}
