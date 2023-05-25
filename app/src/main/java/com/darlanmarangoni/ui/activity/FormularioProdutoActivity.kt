039fed1a955c0c5bee4c1a6d42ef0ea723f6a39d
Darlan Marangoni <marangoni.darlan1@gmail.com>
2023-05-25 08:54:29 -0300

    Configurando listener de click

--- app/src/main/java/com/darlanmarangoni/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/darlanmarangoni/ui/activity/FormularioProdutoActivity.kt
@@ -1,7 +1,23 @@
 package com.darlanmarangoni.ui.activity
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.darlanmarangoni.R
 
-class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val botaoSalvar = findViewById<Button>(R.id.btn_salvar)
+        botaoSalvar.setOnClickListener {
+            val txtNome = findViewById<EditText>(R.id.txt_nome)
+            val nome = txtNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
+    }
+
 }
